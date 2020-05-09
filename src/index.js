b0d0e3b58f0678da10d6b857d2f81f13dd3b9969
rodrigosegatto <Segattorodrigo@hotmail.com>
2020-05-09 15:44:54 -0300

    Iniciando tela da Home. Finalizado o estilo do  Header

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import {StatusBar} from 'react-native'
 
 import Navigation from './navigation';
 
 export default function App() {
   return (
-    <NavigationContainer>
-        <Navigation />
-    </NavigationContainer>
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#000" />
+      <NavigationContainer>
+          <Navigation />
+      </NavigationContainer>
+    </>
   );
 }
 
